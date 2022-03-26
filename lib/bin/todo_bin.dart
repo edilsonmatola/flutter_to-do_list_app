0adf12e57087b56118966c4ac74c30dfabf0da88
Edilson Matola <edilsonmatola13@gmail.com>
2022-03-26 14:28:05 +0200

    Saving and Reading tasks functionality

--- lib/bin/todo_bin.dart
+++ lib/bin/todo_bin.dart
@@ -4,17 +4,27 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 import '../models/todo_model.dart';
 
+// * SharedPreference key
+const todoListKey = 'todo_list';
+
 class TodoBin {
-  TodoBin() {
-    SharedPreferences.getInstance().then(
-      (value) => sharedPreferences = value,
-    );
-  }
 // * Utilizar dps que instancia for criada
   late SharedPreferences sharedPreferences;
 
+  Future<List<TodoModel>> getTodoList() async {
+    sharedPreferences = await SharedPreferences.getInstance();
+    final jsonString = sharedPreferences.getString(todoListKey) ?? '[]';
+    final jsonDecoded = json.decode(jsonString) as List;
+
+    return jsonDecoded
+        .map(
+          (eachItem) => TodoModel.fromJson(eachItem as Map<String, dynamic>),
+        )
+        .toList();
+  }
+
   void saveTodoTaskList(List<TodoModel> todos) {
     final stringJson = json.encode(todos);
-    sharedPreferences.setString('todo_list', stringJson);
+    sharedPreferences.setString(todoListKey, stringJson);
   }
 }
